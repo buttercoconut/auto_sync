8c4f1211d3c459349159f9096c50bdd8bff81840
Yu TaeWoong <ytw2854@gmail.com>
2018-09-01 12:03:57 +0900

    하위 디렉터리 자동 동기화 작업 중

--- sftp/sftp.py
+++ sftp/sftp.py
@@ -1,9 +1,15 @@
 import os, sys
+import subprocess
 from glob import glob
 from datetime import datetime
 
 dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(dir, '../'))
+
+def subprocess_open_when_shell_true(command):
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (stdoutdata, stderrdata) = popen.communicate()
+    return stdoutdata, stderrdata
 
 class SFTP(object):
 
@@ -24,28 +30,65 @@ class SFTP(object):
         for each in local_file_path:
             self.local_list.append(each.replace(self.local + "/", ""))
 
-    def recv(self):
-        for each in self.dest_list:
-            log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_send.log"), "a")
-            if each not in self.local_list:
-                try:
-                    self.sftp.get(self.dest + "/" + each, self.local + "/" + each)
-                    # log record
-                    log_f.write(str(datetime.now()) + " : " + self.dest + "/" + each + "\n")
-                except OSError:
-                    pass
+    def dir_transfer(self, opt, dir_path, dir_name, filelist):
 
+        if opt == "recv":
+
+            try:
+                self.sftp.get(self.dest + "/" + each, self.local + "/" + each)
+                # log record
+                log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_recv.log"), "a")
+                log_f.write(str(datetime.now()) + " : " + self.dest + "/" + each + "\n")
+                log_f.close()
+            except OSError:
+                subprocess_open_when_shell_true("mkdir " + dir_path + "/" + dir_name)
+                self.dir_transfer()
+
+            return filelist
+
+        elif opt == "send":
+            try:
+                self.sftp.put(self.local + "/" + each, self.dest + "/" + each)
+                # log record
+                log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_send.log"), "a")
+                log_f.write(str(datetime.now()) + " : " + self.local + "/" + each + "\n")
+                log_f.close()
+            except OSError:
+                self.ssh.exec_command("mkdir " + dir_path + "/" + dir_name)
+                self.dir_transfer()
+
+            return filelist
+
+    def recv(self):
+        get_list = []
+        for each in self.dest_list:
+            if each not in self.local_list:
+                if os.path.isdir(self.dest + "/" + each):
+                    subprocess_open_when_shell_true("mkdir " + each[1])
+                    self.dir_transfer("recv", self.local, each, get_list)
+                else:
+                    get_list.append([self.dest + "/" + each, self.local + "/" + each])
+
+        for each in get_list:
+            self.sftp.get(each[0], each[1])
+            # log record
+            log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_recv.log"), "a")
+            log_f.write(str(datetime.now()) + " : " + each[0] + "\n")
             log_f.close()
 
     def send(self):
+        put_list = []
         for each in self.local_list:
-            log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_send.log"), "a")
             if each not in self.dest_list:
-                try:
-                    self.sftp.put(self.local + "/" + each, self.dest + "/" + each)
-                    # log record
-                    log_f.write(str(datetime.now()) + " : " + self.local + "/" + each + "\n")
-                except OSError:
-                    pass
+                if os.path.isdir(self.local + "/" + each):
+                    self.ssh.exec_command("mkdir " + each[1])
+                    self.dir_transfer("send", self.dest, each, put_list)
+                else:
+                    put_list.append([self.local + "/" + each, self.dest + "/" + each])
 
+        for each in put_list:
+            self.sftp.put(each[0], each[1])
+            # log record
+            log_f = open(os.path.join(dir[:-5], "log/" + str(datetime.now())[:-16] + "_send.log"), "a")
+            log_f.write(str(datetime.now()) + " : " + each[0] + "\n")
             log_f.close()
